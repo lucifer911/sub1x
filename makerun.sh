bdf7e00aaf6d79aa8255447277a2368cfab2e019
lucifer911 <balagmj@gmail.com>
2018-03-04 14:38:59 -0500

    Update makerun.sh

--- makerun.sh
+++ makerun.sh
@@ -2,7 +2,7 @@
 # makerun.sh
 # Make sure smartcashd is always running.
 # Add the following to the crontab (i.e. crontab -e)
-# */5 * * * * ~/smartnode/makerun.sh
+# */5 * * * * ~/zSub1x/makerun.sh
 
 if ps -A | grep zsub1xd > /dev/null
 then
